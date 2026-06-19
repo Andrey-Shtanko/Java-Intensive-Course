719fdb6ebd99a4558de1b6cb9d7274970c693b10
Andrii Shtanko <andrii.shtanko@pitchbook.com>
2026-06-19 09:57:11 +0300

    HW-1

---
+++ src/com/pitchbook/Area.java
@@ -0,0 +1,36 @@
+package com.pitchbook;
+
+import java.util.Scanner;
+
+public class Area {
+    public static final double PI = 3.1415926;
+
+    public static void areaOfSquare (int side) {
+        System.out.println("The area of the square is: " + side * side);
+    }
+
+    public static void areaOfCircle (int side) {
+        System.out.println("The area of the circle is:" + side * side * PI);
+    }
+
+    public static void areaOfTriangle (int side) {
+        System.out.println("The area of the triangle is:" + (Math.sqrt(3) / 4) * side * side);
+    }
+
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int number = input.nextInt();
+
+        if (number % 2 == 0) {
+            areaOfSquare(number);
+        }
+        else if (number % 3 == 0) {
+            areaOfCircle(number);
+        }
+        else {
+            areaOfTriangle(number);
+        }
+    }
+}
