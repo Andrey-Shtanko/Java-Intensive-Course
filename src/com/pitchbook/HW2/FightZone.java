8fe1466ac51234a21f8bda8312e30a4960988ebf
Andrii Shtanko <andrii.shtanko@pitchbook.com>
2026-06-21 14:34:53 +0300

    HW-2 fight-in-console

--- src/com/pitchbook/HW2/FightZone.java
+++ src/com/pitchbook/HW2/FightZone.java
@@ -4,11 +4,11 @@ public class FightZone {
 
     static void main(String[] args) {
         Weapon bowAndArrows = new Weapon("BowAndArrows", 15);
-        Weapon Knife = new Weapon("Knife", 5);
+        Weapon knife = new Weapon("Knife", 5);
 
         Character rambo = new Character("Rambo", 100, bowAndArrows);
         Character commando = new Character("Commando", 100);
-        Character commandoWithKnife = new Character("Commando", 100, Knife);
+        Character commandoWithKnife = new Character("Commando", 100, knife);
 
         rambo.fight(commando);
         rambo.fight(commandoWithKnife);
